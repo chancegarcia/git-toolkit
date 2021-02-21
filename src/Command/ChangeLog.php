314ce4cbd70305b4b38ac8bd856cb3ee55a56785
chance garcia <develop@chancegarcia.com>
2021-02-21 16:31:29 -0500

    chore: initial files

---
+++ src/Command/ChangeLog.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package
+ * @subpackage
+ * @author      Chance Garcia <chance@garcia.codes>
+ * @copyright   (C)Copyright 2013-2021 Chance Garcia, chancegarcia.com
+ *
+ *    The MIT License (MIT)
+ *
+ * Copyright (c) 2013-2021 Chance Garcia
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ChangeLog extends Command
+{
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'version:changelog';
+
+    protected function configure()
+    {
+        $this
+            // the short description shown while running "php bin/console list"
+            ->setDescription('generate changelog from git commit history')
+
+            // the full command description shown when running the command with
+            // the "--help" option
+            ->setHelp('This command allows you to generate a changelog from your git commit history')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // ... put here the code to create the user
+
+        // this method must return an integer number with the "exit status code"
+        // of the command.
+
+        // return this if there was no problem running the command
+        return 0;
+
+        // or return this if some error happened during the execution
+        // return 1;
+    }
+}
