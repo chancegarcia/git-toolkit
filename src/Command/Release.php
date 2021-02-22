b8cb0e405b2233f2335a40302daf76e4f5cfcb25
chance garcia <develop@chancegarcia.com>
2021-02-21 20:28:45 -0500

    chore: skeleton release command

--- src/Command/Release.php
+++ src/Command/Release.php
@@ -29,7 +29,34 @@
  *
  */
 
-class Release
-{
+namespace Chance\Version\Command;
 
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Release extends Command
+{
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'version:release';
+
+    protected function configure()
+    {
+        // ...
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // ... put here the code to do the stuff
+        $output->writeln('todo: make this a real command.');
+
+        // this method must return an integer number with the "exit status code"
+        // of the command.
+
+        // return this if there was no problem running the command
+        // return 0;
+
+        // or return this if some error happened during the execution
+        return 1;
+    }
 }
